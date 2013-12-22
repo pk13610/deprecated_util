f375b517423e0d82eef543dc1b295abfa3e26f65
pk <pk13610@163.com>
2013-12-23 04:29:36 +0800

    update and add

--- backup2dp.py
+++ backup2dp.py
@@ -2,28 +2,29 @@
 # coding=utf-8
 
 import os
-import sys
-import gzip
+from sys import argv
 import zipfile
-import time
+from time import localtime, strftime
+
 
 def zip_file(src, dest):
     if src == dest:
-        raise "src == dest"
+        raise ValueError(r"src == dest")
     zf = zipfile.ZipFile(dest, "w", zipfile.zlib.DEFLATED)
     zf.write(src, os.path.basename(dest))
     zf.close()
- 
+
+
 def zip_dir(dirname, zipfilename, ignore={}, linkfile=True):
     tmp = zipfilename + '!'
     try:
         filelist = []
         if os.path.isfile(dirname):
             filelist.append(dirname)
-        else :
+        else:
             for root, dirs, files in os.walk(dirname):
-                for name in files:
-                    filelist.append(os.path.join(root, name))
+                for fname in files:
+                    filelist.append(os.path.join(root, fname))
         zf = zipfile.ZipFile(tmp, "w", zipfile.zlib.DEFLATED)
 
         for tar in filelist:
@@ -39,7 +40,7 @@ def zip_dir(dirname, zipfilename, ignore={}, linkfile=True):
                         ignored = True
                         break
                 if not ignored:
-                    zf.write(tar,arcname)
+                    zf.write(tar, arcname)
             except Exception as e:
                 print e
         zf.close()
@@ -47,79 +48,52 @@ def zip_dir(dirname, zipfilename, ignore={}, linkfile=True):
     except Exception as e:
         print e
         os.remove(tmp)
- 
+
+
 def unzip_file(zipfilename, unziptodir):
-    if not os.path.exists(unziptodir): os.mkdir(unziptodir, 0777)
+    if not os.path.exists(unziptodir):
+        os.mkdir(unziptodir, 0777)
     zfobj = zipfile.ZipFile(zipfilename)
     for name in zfobj.namelist():
-        name = name.replace('\\','/')
-        
+        name = name.replace('\\', '/')
+
         if name.endswith('/'):
             os.mkdir(os.path.join(unziptodir, name))
-        else:           
+        else:
             ext_filename = os.path.join(unziptodir, name)
-            ext_dir= os.path.dirname(ext_filename)
-            if not os.path.exists(ext_dir) : os.mkdir(ext_dir,0777)
+            ext_dir = os.path.dirname(ext_filename)
+            if not os.path.exists(ext_dir): os.mkdir(ext_dir, 0777)
             outfile = open(ext_filename, 'wb')
             outfile.write(zfobj.read(name))
             outfile.close()
 
-"""
-backup local settings to some place,say dropbox
-"""
 
 home = os.environ['HOME']
-curr_time = lambda : time.strftime("%Y%m%d%H%M%S", time.localtime())
-dropbox = os.path.join(home, "app/dropbox/Dropbox/SysBackup/autobackup")
-
-settings = [
-                {
-                    "name": "user.bash_profile",
-                    "path": os.path.join(home, ".bash_profile"),
-                    "ignore": ['.git'],
-                    "linkfile": False
-                },
-                {
-                    "name": "user.ssh",
-                    "path": os.path.join(home, ".ssh"),
-                    "ignore": ['.git'],
-                    "linkfile": False
-                },
-                {
-                    "name": "user.bin",
-                    "path": os.path.join(home, "bin"),
-                    "ignore": ['.git', '.log'],
-                    "linkfile": False
-                },
-                {
-                    "name": "user.etc",
-                    "path": os.path.join(home, "etc"),
-                    "ignore": ['.git', '.log', 'log'],
-                    "linkfile": False
-                },
-                {
-                    "name": "xampp.etc",
-                    "path": "/Applications/XAMPP/etc",
-                    "ignore": ['.git', '.log', 'log'],
-                    "linkfile": False
-                },
-]
+curr_time = lambda: strftime("%Y%m%d%H%M%S", localtime())
 
 if __name__ == "__main__":
+    import getopt
+    import json
 
-    for setting in settings:
+    cfg_file = __file__[0: __file__.rfind('.')] + r'.json'
+    opts, args = getopt.getopt(argv[1:], "c:")
+    for k, v in opts:
+        if k in ("-c"):
+            cfg_file = os.path.abspath(cfg_file)
+    config = json.load(open(cfg_file))
+    outpath = config["outpath"].replace("%(home)", home)
+    outpath = os.path.join(outpath, curr_time())
+    for setting in config['settings']:
         name = setting['name']
-        path = setting['path']
+        path = setting['path'].replace("%(home)", home)
         ignore = setting['ignore']
         linkfile = setting['linkfile']
         print name, path, ignore
-        outpath = os.path.join(dropbox, curr_time())
         if not os.path.isdir(outpath):
             os.mkdir(outpath)
         if os.path.isdir(path):
-            out = os.path.join(outpath, "d["+name+"].zip")
+            out = os.path.join(outpath, "d[" + name + "].zip")
             zip_dir(path, out, ignore, linkfile)
         if os.path.isfile(path):
-            out = os.path.join(outpath, "f["+name+"].zip")
+            out = os.path.join(outpath, "f[" + name + "].zip")
             zip_file(path, out)
-
